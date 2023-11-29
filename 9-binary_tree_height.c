37a989b321a0ffcba48b35e28769185533b989fb
Hesham <Dev@heshamorabi.com>
2023-11-29 16:10:57 +0200

    Height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - recursively calculates the height of a binary tree
+ * @tree: pointer to the root node of the tree to calculate the height
+ *
+ * Return: height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (!tree || (!tree->left && !tree->right))
+	{
+		return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
