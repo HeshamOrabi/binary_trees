8fda0b028baf46dc48378e5ad064be61bfa0b5bf
Hesham <Dev@heshamorabi.com>
2023-12-01 18:53:55 +0200

    Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,12 +3,12 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to the node to find the sibling
- * 
+ *
  * Return: pointer to sibling node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || !node->parent || (!node->left && !node->right))
+	if (!node || !node->parent)
 		return (NULL);
 
 	if (node->parent->left == node)
