b3407a497751ee7cfb80e03d5ef31dce553f0651
Hesham <Dev@heshamorabi.com>
2023-12-01 14:59:36 +0200

    Is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/**
+ *
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (!tree)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height == right_height)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - recursively calculates the height of a binary tree
+ * @tree: pointer to the root node of the tree to calculate the height
+ *
+ * Return: height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
