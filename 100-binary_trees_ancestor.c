136e17ee87e7855bbab04289c54788c2ba25cc14
Hesham <Dev@heshamorabi.com>
2023-12-01 21:25:45 +0200

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,8 @@
  *
  * Return: pointer to the lowest common ancestor, or NULL
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
 	const binary_tree_t *temp_second;
 
